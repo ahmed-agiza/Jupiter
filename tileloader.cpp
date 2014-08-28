108d4b8992b6283a567c5fd910a6c8830496f7d0
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2014-08-29 02:34:49 +0300

    tile loader again

--- tileloader.cpp
+++ tileloader.cpp
@@ -14,7 +14,7 @@ TileLoader::TileLoader(QWidget *parent, Memory *memory) :
     ui(new Ui::TileLoader)
 {
     this->setWindowTitle("Tile Loader");
-    this->memory = memory;   
+    this->memory = memory;
     ui->setupUi(this);
     filePath = QDir::currentPath();
     ui->lineEdit->setText(filePath);
@@ -89,67 +89,74 @@ void TileLoader::on_pushButton_2_clicked()
 {
     sf::Image imageToLoad;
     QVector< QVector< sf::Uint8 > > imageMatrix(16,QVector<sf::Uint8>(16, 0));
-    imageToLoad.loadFromFile(filePath.toStdString());
-    if(imageToLoad.getSize().x != 16 || imageToLoad.getSize().y != 16){
-        QImage qImageToLoad;
-        QImage qImageAlpha;
-        qImageToLoad.load(filePath);
-        qImageToLoad.convertToFormat(QImage::Format_ARGB32);
-        qImageToLoad = qImageToLoad.scaled(16,16);
-        qImageAlpha = qImageToLoad.alphaChannel();
-        for(int i=0; i<16; i++){
-            for(int j=0; j<16; j++){
-                QColor myQColor(qImageToLoad.pixel(j,i));
-                sf::Color sfmlColor;
-                sfmlColor.r = myQColor.red();
-                sfmlColor.g = myQColor.green();
-                sfmlColor.b = myQColor.blue();
-                sfmlColor.a = qAlpha(qImageAlpha.pixel(j, i));
-                imageToLoad.setPixel(j,i,sfmlColor);
-            }
-        }
-    }
-    int tileIndex = ui->spinBox->value();
-    int tileBaseAddress = ((ui->pushButton_3->text() == "Background")? memory->backgroundTileSetBaseAddress:memory->spritesTileSetBaseAddress);
-    for(int i=0; i<16; i++){
-        for(int j=0; j<16; j++){
-            Uint32 color = colorToInt(imageToLoad.getPixel(j,i));
-            if(paletteSet.contains(color)){
-                imageMatrix[i][j] = paletteSet[color];
-            }else{
-                if(palettePointer<256){
-                    while(palettePointer<256 && memory->palette[palettePointer].getColor() != sf::Color(0,0,0,0)){
-                        palettePointer++;
-                    }
-                    if(palettePointer == 256){
-                        qDebug() << "Error! Color palette is full";
-                    }else{
-                        memory->storeWord(memory->paletteBaseAddress + (palettePointer*4), color);
-                        paletteSet[color] = palettePointer;
-                        imageMatrix[i][j] = palettePointer;
-                    }
-                }else{
-                    qDebug() << "Error! Color palette is full";
+    qDebug() << filePath;
+    if(imageToLoad.loadFromFile(filePath.toStdString())){
+        if(imageToLoad.getSize().x != 16 || imageToLoad.getSize().y != 16){
+            QImage qImageToLoad;
+            QImage qImageAlpha;
+            qImageToLoad.load(filePath);
+            qImageToLoad.convertToFormat(QImage::Format_ARGB32);
+            qImageToLoad = qImageToLoad.scaled(16,16);
+            qImageAlpha = qImageToLoad.alphaChannel();
+            for(int i=0; i<16; i++){
+                for(int j=0; j<16; j++){
+                    QColor myQColor(qImageToLoad.pixel(j,i));
+                    sf::Color sfmlColor;
+                    sfmlColor.r = myQColor.red();
+                    sfmlColor.g = myQColor.green();
+                    sfmlColor.b = myQColor.blue();
+                    sfmlColor.a = qAlpha(qImageAlpha.pixel(j, i));
+                    imageToLoad.setPixel(j,i,sfmlColor);
                 }
             }
         }
-    }
-    for(int i=0; i<16; i++){
-        for(int j=0; j<16; j++){
-            memory->storeByte((tileBaseAddress | (tileIndex << 8) | (i<<4) | j), imageMatrix[i][j]);
+        int tileIndex = ui->spinBox->value();
+        int tileBaseAddress = ((ui->pushButton_3->text() == "Background")? memory->backgroundTileSetBaseAddress:memory->spritesTileSetBaseAddress);
+        for(int i=0; i<16; i++){
+            for(int j=0; j<16; j++){
+                Uint32 color = colorToInt(imageToLoad.getPixel(j,i));
+                if(paletteSet.contains(color)){
+                    imageMatrix[i][j] = paletteSet[color];
+                }else{
+                    if(palettePointer<256){
+                        while(palettePointer<256 && memory->palette[palettePointer].getColor() != sf::Color(0,0,0,0)){
+                            palettePointer++;
+                        }
+                        if(palettePointer == 256){
+                            qDebug() << "Error! Color palette is full";
+                        }else{
+                            memory->storeWord(memory->paletteBaseAddress + (palettePointer*4), color);
+                            paletteSet[color] = palettePointer;
+                            imageMatrix[i][j] = palettePointer;
+                        }
+                    }else{
+                        qDebug() << "Error! Color palette is full";
+                    }
+                }
+            }
         }
-    }
-    if(ui->pushButton_3->text() == "Background"){
-        backgroundTileIndexToLoad++;
-        ui->spinBox->setValue(backgroundTileIndexToLoad);
+        for(int i=0; i<16; i++){
+            for(int j=0; j<16; j++){
+                memory->storeByte((tileBaseAddress | (tileIndex << 8) | (i<<4) | j), imageMatrix[i][j]);
+            }
+        }
+        if(ui->pushButton_3->text() == "Background"){
+            backgroundTileIndexToLoad++;
+            ui->spinBox->setValue(backgroundTileIndexToLoad);
+        }
+        else{
+            spriteTileIndexToLoad++;
+            ui->spinBox->setValue(spriteTileIndexToLoad);
+        }
+        QMessageBox::information(this, tr("Tile Loader"),
+                                 tr("Tile sucessfully loaded"),
+                                 QMessageBox::Ok);
     }
     else{
-        spriteTileIndexToLoad++;
-        ui->spinBox->setValue(spriteTileIndexToLoad);
+        QMessageBox::information(this, tr("Tile Loader"),
+                                 tr("Failed to open tile"),
+                                 QMessageBox::Ok);
     }
-   QMessageBox::information(this, tr("Tile Loader"),
-                                    tr("Tile sucessfully loaded"),
-                                    QMessageBox::Ok);
 }
 
 void TileLoader::on_lineEdit_editingFinished()
